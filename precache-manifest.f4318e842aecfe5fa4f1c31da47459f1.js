8c3a0060094b53413ea95181c5ff055d9916621f
AntonChetveryk <chetverykanton92@gmail.com>
2020-06-04 13:57:01 +0300

    Updates

---
+++ precache-manifest.f4318e842aecfe5fa4f1c31da47459f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d56f84aeeef65705a5c0e45baff2f63",
+    "url": "/jappware/index.html"
+  },
+  {
+    "revision": "769eef083d9ff5918080",
+    "url": "/jappware/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2faa38a56db6af1dc7b1",
+    "url": "/jappware/static/css/main.5b7b4b9d.chunk.css"
+  },
+  {
+    "revision": "769eef083d9ff5918080",
+    "url": "/jappware/static/js/2.0e07e583.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jappware/static/js/2.0e07e583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2faa38a56db6af1dc7b1",
+    "url": "/jappware/static/js/main.1ed06b81.chunk.js"
+  },
+  {
+    "revision": "0bea329687f12017b324",
+    "url": "/jappware/static/js/runtime-main.b589997c.js"
+  }
+]);
